cfb9ac1904fb13c426daaa31baf5d061755b187a
izzett222 <iizzeddin62@gmail.com>
2023-02-20 09:01:26 +0200

    setup nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: '--font-roboto',
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <main className={`${roboto.variable} font-sans`}><Component {...pageProps} /></main>
 }
