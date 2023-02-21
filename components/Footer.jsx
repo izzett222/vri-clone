21cfe010bdc6f9efd98d82e1346d86d42efd8ebd
izzett222 <iizzeddin62@gmail.com>
2023-02-21 18:49:29 +0200

    add wrapper on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,6 @@
-import right from "/public/white-right.svg"
+import right from "/public/white-right.svg";
 import Image from "next/image";
+import IWrapper from "./IWrapper";
 export default function Footer() {
   const data = [
     "Privacy Statement",
@@ -9,15 +10,26 @@ export default function Footer() {
     "Cookie Settings",
     "Web archive",
   ];
-  return <footer className="bg-primary py-[30px] px-5 sm:px-[30px]">
-    <div className="flex gap-x-3 gap--1 flex-wrap 2xl:mx-[130px]">
-        {data.map((el, i) => {
-            return <div key={i} className="text-white leading-[27px] flex items-center gap-1">
+  return (
+    <footer className="bg-primary py-[30px]">
+      <IWrapper>
+        <div className="flex gap-x-3 gap--1 flex-wrap">
+          {data.map((el, i) => {
+            return (
+              <div
+                key={i}
+                className="text-white leading-[27px] flex items-center gap-1"
+              >
                 <span>{el}</span>
                 <Image src={right} className="w-4" alt="" />
-            </div>
-        })}
-    </div>
-    <p className="text-white mt-5 2xl:mx-[130px]">Copyright © 2023 - Vrije Universiteit Amsterdam</p>
-  </footer>;
+              </div>
+            );
+          })}
+        </div>
+        <p className="text-white mt-5">
+          Copyright © 2023 - Vrije Universiteit Amsterdam
+        </p>
+      </IWrapper>
+    </footer>
+  );
 }
